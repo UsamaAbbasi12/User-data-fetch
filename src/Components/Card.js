ce00c7a493b974d008eb942e5f8cea3ddb8b61f7
UsamaAbbasi12 <usamaabbasi199@gmail.com>
2022-01-17 16:43:08 +0500

    second commit

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from "react";
 import Card1 from "./Card1";
-import Search from "./Search";
+// import Search from "./Search";
 import "../styles/Card1.scss";
 
 function Card() {
   const [data, setData] = useState([]);
+  const [searchItem, setSearch] = useState("");
+  const styles = {
+    width: "400px",
+    padding: "15px",
+    margin: "10px auto",
+    display: "block",
+    border: "2px solid gray",
+    outline: "none",
+    borderRadius: "8px",
+  };
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -13,14 +23,34 @@ function Card() {
         setData(users);
       });
   }, []);
-
+  const changeHandler = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+  };
   return (
     <div className="userMain">
-      <Search />
       <div className="userCards">
-        {data.map((user) => (
-          <Card1 name={user.name} email={user.email} key={user.name} />
-        ))}
+        <input
+          type="text"
+          value={searchItem}
+          placeholder="Enter Name to Search"
+          style={styles}
+          onChange={changeHandler}
+        />
+
+        {data
+          .filter((val) => {
+            if (searchItem === "") {
+              return val;
+            } else if (
+              searchItem ===
+              val.name.toLowerCase().includes(searchItem.toLowerCase())
+            ) {
+            }
+          })
+          .map((user) => (
+            <Card1 name={user.name} email={user.email} key={user.name} />
+          ))}
       </div>
     </div>
   );
